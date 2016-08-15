0caad6f630ed677eb246033f718022ca36df85a7
ice1000 <ice1000kotlin@foxmail.com>
2016-08-15 23:24:12 +0800

    now you can cut a part from an existing image resource.

---
+++ src/org/frice/game/resource/image/PartImageResource.kt
@@ -0,0 +1,14 @@
+package org.frice.game.resource.image
+
+import java.awt.Rectangle
+
+/**
+ * Created by ice1000 on 2016/8/15.
+ * @author ice1000
+ * @since v0.2.3
+ */
+class PartImageResource(origin: ImageResource, x: Int, y: Int, width: Int, height: Int) : ImageResource() {
+	constructor(origin: ImageResource, part: Rectangle) : this(origin, part.x, part.y, part.width, part.height)
+
+	override var image = origin.image.getSubimage(x, y, width, height)
+}
