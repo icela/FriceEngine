ae91069c7a4e4a9358a768df0c28ce06eead2c7e
ice1000 <ice1000kotlin@foxmail.com>
2016-08-15 14:30:26 +0800

    simplify demo

--- demo/Demo2.java
+++ demo/Demo2.java
@@ -6,9 +6,6 @@ import org.frice.game.event.OnMouseEvent;
 import org.frice.game.event.OnWindowEvent;
 import org.frice.game.obj.ImageObject;
 import org.frice.game.resource.FileImageResource;
-import org.frice.game.utils.time.FTimer;
-
-import java.util.Random;
 
 /**
  * Created by ice1000 on 2016/8/15.
@@ -18,24 +15,18 @@ import java.util.Random;
  */
 public class Demo2 extends Game {
 
-	private FTimer timer;
-	private Random random = new Random();
-	private ImageObject object;
-
 	public static void main(String[] args) {
 		new Demo2();
 	}
 
 	@Override
 	public void onInit() {
-		timer = new FTimer(10000);
 		setSize(800, 800);
-		object = new ImageObject(new FileImageResource("test.png"), 0, 620);
+		ImageObject object = new ImageObject(new FileImageResource("test.png"), 0, 620);
 		object.getAnims().add(new ForceMove(0, 10));
 		object.getAnims().add(new SimpleMove(0, -600));
 		object.getAnims().add(new SimpleMove(100, 0));
 		addObject(object);
-//		setBack(new FileImageResource("test.png"));
 		setRefreshPerSecond(40);
 	}
 
@@ -46,19 +37,10 @@ public class Demo2 extends Game {
 
 	@Override
 	public void onRefresh() {
-//		try {
-//			if (timer.ended()) {
-//				object.getAnims().clear();
-//				object.getAnims().add(new SimpleMove(random.nextInt(180) - 90, random.nextInt(180) - 90));
-//				object.getAnims().add(new ForceMove(random.nextInt(180) - 90, random.nextInt(180) - 90));
-//			}
-//		} catch (Exception ignored){
-//		}
 	}
 
 	@Override
 	public void onClick(OnClickEvent e) {
-
 	}
 
 	@Override
