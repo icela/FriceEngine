7f4bbb23af53cbcadc18c1378b7b42c98f5de707
ice1000 <ice1000kotlin@foxmail.com>
2016-08-16 21:05:09 +0800

    fuck this

--- demo/Demo8.java
+++ demo/Demo8.java
@@ -1,7 +1,7 @@
 import org.frice.game.Game;
 import org.frice.game.obj.ImageObject;
+import org.frice.game.resource.image.FileImageResource;
 import org.frice.game.resource.image.FrameImageResource;
-import org.frice.game.resource.image.WebImageResource;
 
 /**
  * Created by ice1000 on 2016/8/16.
@@ -14,12 +14,12 @@ public class Demo8 extends Game {
 
 	@Override
 	protected void onInit() {
-		object = new ImageObject(new FrameImageResource(new WebImageResource[]{
-				new WebImageResource("1.png"),
-				new WebImageResource("2.png"),
-				new WebImageResource("3.png"),
-				new WebImageResource("4.png"),
-				new WebImageResource("5.png")}, 1000), 100.0, 100.0);
+		object = new ImageObject(new FrameImageResource(new FileImageResource[]{
+				new FileImageResource("1.png"),
+				new FileImageResource("2.png"),
+				new FileImageResource("3.png"),
+				new FileImageResource("4.png"),
+				new FileImageResource("5.png")}, 1000), 100.0, 100.0);
 		addObject(object);
 	}
 
--- src/org/frice/game/resource/image/FrameImageResource.kt
+++ src/org/frice/game/resource/image/FrameImageResource.kt
@@ -1,5 +1,7 @@
 package org.frice.game.resource.image
 
+import org.frice.game.Game
+import org.frice.game.utils.message.log.FLog
 import org.frice.game.utils.time.FTimeListener
 import java.awt.image.BufferedImage
 
@@ -8,23 +10,38 @@ import java.awt.image.BufferedImage
  * @author ice1000
  * @since v0.3.1
  */
-class FrameImageResource(val list: MutableList<ImageResource>, val div: Int, val loop: Boolean) : ImageResource() {
-	constructor(list: MutableList<ImageResource>, div: Int) : this(list, div, true)
-	constructor(list: Array<ImageResource>, div: Int, loop: Boolean) : this(list.toMutableList(), div, loop)
-	constructor(list: Array<ImageResource>, div: Int) : this(list.toMutableList(), div, true)
+class FrameImageResource(val game: Game, val list: MutableList<ImageResource>, div: Int, val loop: Boolean) : ImageResource() {
+	constructor(game: Game, list: MutableList<ImageResource>, div: Int) :
+	this(game, list, div, true)
+
+	constructor(game: Game, list: Array<ImageResource>, div: Int, loop: Boolean) :
+	this(game, list.toMutableList(), div, loop)
+
+	constructor(game: Game, list: Array<ImageResource>, div: Int) :
+	this(game, list.toMutableList(), div, true)
 
 	private var start: Long
 	private val timer: FTimeListener
 	private var counter = 0
+	private var ended = false
 
 	override var image: BufferedImage
-		get() = list[counter % list.size].image
+		get() = if (loop || ended) list.last().image else list.getImage(counter).image
 		set(value) {
 			list.add(ImageResource.create(value))
 		}
 
+	fun MutableList<ImageResource>.getImage(index: Int): ImageResource {
+		if (index == this.size - 1) ended = true
+		return this[index]
+	}
+
 	init {
 		start = System.currentTimeMillis()
-		timer = FTimeListener(div, { counter++ })
+		timer = FTimeListener(div, {
+			FLog.e("fuck")
+			counter++
+			counter %= list.size
+		})
 	}
 }
