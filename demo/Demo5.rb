23e922c795b38fae83aed14e63e9f3dc9c066f5b
ice1000 <ice1000kotlin@foxmail.com>
2016-08-16 10:49:35 +0800

    fuck JRuby

--- demo/Demo5.rb
+++ demo/Demo5.rb
@@ -1,18 +1,38 @@
+# This demo doesn't work.
+# I expect to extend a java class but it seemed not working.
+# Is this the problem of my JRuby environment?
+
 require 'java'
 
-class Demo5 < org.frice.game.GameForRuby
-	def initialize
-		onInit
-	end
+# change the import path in your own one
+# don't use this path on your computer
+require 'FriceEngine.jar'
+
+class Demo5 < org.frice.game.Game
 	def onInit
-		initWindow
-		window.setBounds java.awt.Rectangle.new 100, 100, 800, 800
-		window.setTitle 'JRuby demo by ice1000'
+		setBounds java.awt.Rectangle.new 100, 100, 800, 800
+		setTitle 'JRuby demo by ice1000'
+
+		@bool = false
+
+		@oval = org.frice.game.utils.shape.FOval.new 20.0, 15.0
+		@obj = org.frice.game.obj.ShapeObject.new org.frice.game.resource.ColorResource.Companion.BLUE, @oval
+		@timer = org.frice.game.utils.time.FTimer.new 1000
 	end
+
 	def on_refresh
+		if @timer.ended
+			if @bool
+				remove_object @obj
+			else
+				add_object @obj
+			end
+		end
 	end
+
 	def on_click(e)
 	end
+
 	def on_mouse(e)
 	end
 end
