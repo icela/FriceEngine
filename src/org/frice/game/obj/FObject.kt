50952e7100069776872f3c184769c87f5584eac0
ice1000 <ice1000kotlin@foxmail.com>
2016-08-19 19:48:35 +0800

    fix hierarchy

--- src/org/frice/game/obj/FObject.kt
+++ src/org/frice/game/obj/FObject.kt
@@ -14,16 +14,16 @@ import java.util.*
  * @author ice1000
  * @since v0.1
  */
-interface FObject : CollideBox , PhysicalObject{
+interface FObject : CollideBox, PhysicalObject {
 	var id: Int
 	val anims: ArrayList<FAnim>
-	val targets: ArrayList<Pair<FObject, OnCollideEvent>>
+	val targets: ArrayList<Pair<PhysicalObject, OnCollideEvent>>
 	val collideBox: FShape
 	fun getResource(): FResource
 	fun move(p: Pair<Double, Double>)
 	fun scale(p: Pair<Double, Double>)
 
-	fun rectCollide(rect1: FObject, rect2: FObject) = rect1.x + rect1.width >= rect2.x &&
+	fun rectCollide(rect1: PhysicalObject, rect2: PhysicalObject) = rect1.x + rect1.width >= rect2.x &&
 			rect2.y <= rect1.y + rect1.height &&
 			rect1.x <= rect2.x + rect2.width &&
 			rect1.y <= rect2.y + rect2.height
