f7b9a9a6d0f3faa72b8f5e636c18a10ec5c30181
ice1000 <ice1000kotlin@foxmail.com>
2016-08-21 20:49:31 +0800

    making a demo which has used all apis in frice

--- test/org/frice/game/utils/data/Test.kt
+++ test/org/frice/game/utils/data/Test.kt
@@ -1,9 +1,17 @@
 package org.frice.game.utils.data
 
 import org.frice.game.Game
+import org.frice.game.anim.move.AccelerateMove
+import org.frice.game.anim.move.SimpleMove
+import org.frice.game.anim.scale.SimpleScale
+import org.frice.game.event.OnClickEvent
+import org.frice.game.obj.button.OnClickListener
+import org.frice.game.obj.button.SimpleButton
 import org.frice.game.obj.sub.ShapeObject
 import org.frice.game.resource.graphics.ColorResource
 import org.frice.game.utils.graphics.shape.FCircle
+import org.frice.game.utils.graphics.shape.FRectangle
+import org.frice.game.utils.message.log.FLog
 
 /**
  * Created by ice1000 on 2016/8/21.
@@ -13,16 +21,28 @@ import org.frice.game.utils.graphics.shape.FCircle
 class Test() : Game() {
 	override fun onInit() {
 		super.onInit()
-
-//		FLog.i(FDialog(this).confirm("fuck"))
-		addObject(ShapeObject(ColorResource.西木野真姬, FCircle(40.0), 100.0, 100.0).apply {
-//			anims.add()
+		addObject(SimpleButton(FRectangle(80, 20), "I am a button", 30.0, 30.0, 80.0, 30.0).apply {
+			onClickListener = object : OnClickListener {
+				override fun onClick() {
+					addObject(ShapeObject(ColorResource.西木野真姬, FCircle(40.0), 100.0, 100.0).apply {
+						anims.add(SimpleMove(150, 150))
+						anims.add(AccelerateMove(-1.0, -1.0))
+						anims.add(SimpleScale(1.1, 1.0))
+					})
+				}
+			}
 		})
 	}
 
-//	override fun onExit() {
-//		System.exit(0)
-//	}
+	override fun onClick(e: OnClickEvent?) {
+		super.onClick(e)
+		FLog.v(e.toString())
+		FLog.v(mousePosition)
+	}
+
+	override fun onExit() {
+		System.exit(0)
+	}
 }
 
 fun main(args: Array<String>) {
