d4a884c17f15457fe1e9ffb2cc2fce6dfa622f56
ice1000 <ice1000kotlin@foxmail.com>
2016-08-27 04:26:25 +0800

    finish

--- src/org/frice/game/obj/effects/FunctionEffect.kt
+++ src/org/frice/game/obj/effects/FunctionEffect.kt
@@ -3,19 +3,35 @@ package org.frice.game.obj.effects
 import org.frice.game.obj.sub.ImageObject
 import org.frice.game.resource.graphics.ColorResource
 import org.frice.game.resource.graphics.FunctionResource
+import org.frice.game.resource.image.ImageResource
+import java.awt.Image
+import java.awt.image.BufferedImage
 
 /**
+ * Not tested
  * Created by ice1000 on 2016/8/24.
  *
  * @author ice1000
  * @since v0.4.1
  */
-class FunctionEffect(f: FunctionResource, override var x: Double, override var y: Double) :
-		ImageObject(f.getResource(), x, y) {
+class FunctionEffect(res: FunctionResource, override var x: Double, override var y: Double) :
+		ImageObject(res.getResource(), x, y) {
 
 	constructor(function: Function, x: Double, y: Double, width: Int, height: Int) :
 	this(FunctionResource(ColorResource.BLUE, { x -> function.call(x) }, width, height), x, y)
 
+	override val width: Double
+		get() = res.image.width.toDouble()
+	override val height: Double
+		get() = res.image.height.toDouble()
 
+	override fun getResource() = ImageResource.create(getImage())
+
+	override fun getImage() = res.image
+
+	override fun scale(p: Pair<Double, Double>) {
+		res.image = res.image.getScaledInstance((res.image.width * p.first).toInt(),
+				(res.image.height * p.second).toInt(), Image.SCALE_DEFAULT) as BufferedImage
+	}
 
 }
