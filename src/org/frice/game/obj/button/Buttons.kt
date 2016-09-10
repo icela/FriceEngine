3a88d04ff28d61d3de30edd4479b6f2b77e681e0
master <951394653@qq.com>
2016-09-10 11:28:55 +0800

    change source code structure

---
+++ src/org/frice/game/obj/button/Buttons.kt
@@ -0,0 +1,102 @@
+package org.frice.game.obj.button
+
+import org.frice.game.event.OnClickEvent
+import org.frice.game.event.OnMouseEvent
+import org.frice.game.obj.FContainer
+import org.frice.game.obj.FObject
+import org.frice.game.resource.graphics.ColorResource
+import org.frice.game.resource.image.ImageResource
+import java.awt.Font
+
+/**
+ * Created by ice1000 on 2016/8/18.
+ * @author ice1000
+ * @since v0.3.2
+ */
+
+interface FButton : FContainer {
+	var onClickListener: OnClickListener?
+
+	infix fun onClick(e: OnClickEvent) = onClickListener?.onClick(e)
+
+	/**
+	 * @return true means pressed
+	 */
+	infix fun onMouse(e: OnMouseEvent) = (e.type() == OnMouseEvent.MOUSE_PRESSED)
+
+	/**
+	 * Created by ice1000 on 2016/8/19.
+	 * @author ice1000
+	 * @since v0.4
+	 */
+	interface OnClickListener {
+		fun onClick(e: OnClickEvent)
+	}
+
+	/**
+	 * Created by ice1000 on 2016/8/19.
+	 * @author ice1000
+	 * @since v0.4
+	 */
+	interface OnMouseListener {
+		fun onMouse(e: OnMouseEvent)
+	}
+}
+
+
+/**
+ * Created by ice1000 on 2016/9/3 0003.
+ *
+ * @author ice1000
+ * @since v0.5
+ */
+class ImageButton(val imageNormal: ImageResource, val imagePressed: ImageResource,
+                  override var x: Double, override var y: Double,
+                  override var width: Double, override var height: Double) : FButton, FObject.ImageOwner {
+	constructor(image: ImageResource, x: Double, y: Double, width: Double, height: Double) :
+	this(image, image, x, y, width, height)
+
+	override var onClickListener: FButton.OnClickListener? = null
+
+	private var bool = false
+
+	override fun getImage() = if (bool) imagePressed.image else imageNormal.image
+
+	override fun onMouse(e: OnMouseEvent): Boolean {
+		bool = super.onMouse(e)
+		return bool
+	}
+}
+
+/**
+ * Created by ice1000 on 2016/8/18.
+ *
+ * @author ice1000
+ * @since v0.3.3
+ */
+class SimpleButton(var colorResource: ColorResource,
+                   override var text: String, override var font: Font,
+                   override var x: Double, override var y: Double,
+                   override var width: Double, override var height: Double) : FButton, FText() {
+
+	constructor(text: String, font: Font, x: Double, y: Double,
+	            width: Double, height: Double) : this(ColorResource.GRAY, text, font, x, y, width, height)
+
+	constructor(text: String, font: Int, x: Double, y: Double, width: Double, height: Double) :
+	this(text, Font(Font.MONOSPACED, Font.BOLD, font), x, y, width, height)
+
+	constructor(text: String, x: Double, y: Double, width: Double, height: Double) :
+	this(text, Font(Font.MONOSPACED, Font.BOLD, 16), x, y, width, height)
+
+	private var bool = false
+
+	override var onClickListener: FButton.OnClickListener? = null
+
+	override fun getColor() = if (bool) ColorResource(colorResource.color.darker())
+	else colorResource
+
+	override fun onMouse(e: OnMouseEvent): Boolean {
+		bool = super.onMouse(e)
+		return bool
+	}
+}
--- src/org/frice/game/obj/effects/Effects.kt
+++ src/org/frice/game/obj/effects/Effects.kt
@@ -1,11 +1,54 @@
 package org.frice.game.obj.effects
 
+import org.frice.game.obj.AbstractObject
 import org.frice.game.obj.sub.ImageObject
 import org.frice.game.resource.graphics.ColorResource
 import org.frice.game.resource.graphics.FunctionResource
+import org.frice.game.resource.graphics.ParticleResource
 import org.frice.game.resource.image.ImageResource
+import org.frice.game.utils.graphics.shape.FRectangle
 import java.awt.Image
 import java.awt.image.BufferedImage
+
+/**
+ * Created by ice1000 on 2016/8/19.
+ *
+ * @author ice1000
+ * @since v0.5
+ */
+class LineEffect(var colorResource: ColorResource, override var x: Double, override var y: Double,
+                 var x2: Double, var y2: Double) : AbstractObject {
+
+	override var rotate = 0.0
+
+	constructor(x: Double, y: Double, x2: Double, y2: Double) : this(ColorResource.BLACK, x, y, x2, y2)
+}
+
+/**
+ * Created by ice1000 on 2016/8/17.
+ * @author ice1000
+ * @since 0.3.2
+ */
+class ParticleEffect(private var resource: ParticleResource, override var x: Double, override var y: Double) :
+		ImageObject(resource.getResource(), x, y) {
+	override val collideBox = FRectangle(x.toInt(), y.toInt())
+
+	override val width: Double
+		get() = resource.x.toDouble()
+	override val height: Double
+		get() = resource.y.toDouble()
+
+	override fun getImage() = resource.getResource()
+
+	override fun getResource() = ImageResource.create(getImage())
+
+	override fun scale(p: Pair<Double, Double>) {
+		resource.x = (resource.x * p.first).toInt()
+		resource.y = (resource.y * p.second).toInt()
+	}
+
+	//	override fun isCollide(other: CollideBox): Boolean = false
+}
 
 /**
  * Tested, Work stably.
