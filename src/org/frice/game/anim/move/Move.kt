b557626f5e8e29ecde1825d98e5ff5d5e5b41d3e
master <951394653@qq.com>
2016-09-15 10:20:31 +0800

    a group of operator

--- src/org/frice/game/anim/move/Move.kt
+++ src/org/frice/game/anim/move/Move.kt
@@ -56,19 +56,46 @@ data class DoublePair(var x: Double, var y: Double) {
 		@JvmStatic fun from1000(x: Double, y: Double) = DoublePair(x / 1000.0, y / 1000.0)
 	}
 
-	operator fun plusAssign(double: DoublePair) {
-		x += double.x
-		y += double.y
+	operator fun plusAssign(d: DoublePair) {
+		x += d.x
+		y += d.y
 	}
 
-	operator fun minusAssign(double: DoublePair) {
-		x -= double.x
-		y -= double.y
+	operator fun plusAssign(d: Double) {
+		x += d
+		y += d
 	}
 
-	operator fun div(double: Double) = DoublePair(x / double, y / double)
+	operator fun minusAssign(d: DoublePair) {
+		x -= d.x
+		y -= d.y
+	}
 
-	operator fun times(double: Double) = DoublePair(x * double, y * double)
+	operator fun minusAssign(d: Double) {
+		x -= d
+		y -= d
+	}
+
+	operator fun timesAssign(d: Double) {
+		x *= d
+		y *= d
+	}
+
+	operator fun divAssign(d: Double) {
+		x /= d
+		y /= d
+	}
+
+	operator fun div(d: Double) = DoublePair(x / d, y / d)
+	operator fun times(d: Double) = DoublePair(x * d, y * d)
+	operator fun plus(d: Double) = DoublePair(x + d, y + d)
+	operator fun plus(d: DoublePair) = DoublePair(x + d.x, y + d.y)
+	operator fun minus(d: Double) = DoublePair(x - d, y - d)
+	operator fun minus(d: DoublePair) = DoublePair(x - d.x, y - d.y)
+	operator fun inc() = DoublePair(x++, y++)
+	operator fun dec() = DoublePair(x--, y--)
+	operator fun unaryPlus() = DoublePair(x, y)
+	operator fun unaryMinus() = DoublePair(-x, -y)
 }
 
 
