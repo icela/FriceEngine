f75f286432b79f61dec9da58b533dd350a876b6b
ice1000 <ice1000kotlin@foxmail.com>
2016-09-15 23:13:33 +0800

    add dialog utilities.

--- src/org/frice/game/utils/message/FDialog.kt
+++ src/org/frice/game/utils/message/FDialog.kt
@@ -1,5 +1,8 @@
 package org.frice.game.utils.message
 
+import com.eldath.alerts.ErrorAlert
+import com.eldath.alerts.InfoAlert
+import com.eldath.alerts.WarnAlert
 import java.awt.Frame
 import javax.swing.JOptionPane
 
@@ -15,4 +18,12 @@ class FDialog(val game: Frame?) {
 	fun confirm(msg: String) = confirm(msg, "")
 	fun confirm(msg: String, title: String) = confirm(msg, title, JOptionPane.YES_NO_CANCEL_OPTION)
 	fun confirm(msg: String, title: String, option: Int) = JOptionPane.showConfirmDialog(game, msg, title, option)
+
+	companion object {
+		@JvmStatic fun errorDialog(doWhat: String, msg: String) = ErrorAlert(doWhat, msg)
+		@JvmStatic fun errorDialog(doWhat: String, throwable: Throwable) = ErrorAlert(doWhat, throwable)
+		@JvmStatic fun infoDialog(msg: String) = InfoAlert(msg)
+		@JvmStatic fun warnDialog(doWhat: String, msg: String) = WarnAlert(doWhat, msg)
+		@JvmStatic fun warnDialog(doWhat: String, throwable: Throwable) = WarnAlert(doWhat, throwable)
+	}
 }
