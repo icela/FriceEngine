8febacc40969ecd5884452ab81b9b833b8e94a0f
ice1000 <ice1000kotlin@foxmail.com>
2016-10-23 16:57:47 +0800

    我彻底懵逼了 调了一天

--- test/org/frice/game/Test3.kt
+++ test/org/frice/game/Test3.kt
@@ -13,17 +13,18 @@ import org.frice.game.utils.graphics.shape.FCircle
 
 class Test3() : Game() {
 	lateinit var a: ShapeObject
-	val d = 3.0
-	val c = 0.2
+	val d = 3.14 * 6
+	val e = 0.1
+	val c = 0.1
 	override fun onInit() {
 		setSize(1000, 1000)
-		a = ShapeObject(ColorResource.BLUE, FCircle(50.0), 50.0, 500.0)
+		a = ShapeObject(ColorResource.BLUE, FCircle(10.0), 100.0, 500.0)
 		a.anims.add(object : CustomMove() {
 			override fun getXDelta(timeFromBegin: Double) =
-					((a.x * c) * Math.sin(d) - (a.y * c) * Math.cos(d)) / 100
+					((a.x * c) * Math.sin(d) - (a.y * c) * Math.cos(d)) * e
 
 			override fun getYDelta(timeFromBegin: Double) =
-					((a.x * c) * Math.cos(d) - (a.y * c) * Math.sin(d)) / 100
+					((a.x * c) * Math.cos(d) - (a.y * c) * Math.sin(d)) * e
 		})
 		addObject(a)
 	}
