0ed225c8b28dbba0e2b26fe1e719e71ff0eb04c3
ice1000 <ice1000kotlin@foxmail.com>
2017-07-22 17:54:59 +0800

    Update managers when writing resource

--- src/org/frice/game/utils/data/FileUtils.kt
+++ src/org/frice/game/utils/data/FileUtils.kt
@@ -5,26 +5,48 @@
  * @since v0.5
  */
 @file:JvmName("FileUtils")
+
 package org.frice.game.utils.data
 
+import org.frice.game.platform.adapter.JvmImage
+import org.frice.game.resource.manager.FileBytesManager
+import org.frice.game.resource.manager.FileTextManager
+import org.frice.game.resource.manager.ImageManager
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
-fun BufferedImage.image2File(type: String, file: File) = ImageIO.write(this, type, file)
+fun BufferedImage.image2File(type: String, file: File) = JvmImage(this).image2File(type, file)
+
+fun JvmImage.image2File(type: String, file: File) {
+	ImageIO.write(image, type, file)
+	ImageManager[file.path] = this
+}
 
 fun BufferedImage.image2File(file: File) = image2File("png", file)
+fun JvmImage.image2File(file: File) = image2File("png", file)
 
 fun BufferedImage.image2File(file: String) = image2File(File(file))
+fun JvmImage.image2File(file: String) = image2File(File(file))
 
-fun String.string2File(file: File) = file.writeText(this)
+fun String.string2File(file: File) {
+	file.writeText(this)
+	FileTextManager[file.path] = this
+}
 
 fun String.string2File(file: String) = string2File(File(file))
 
-fun ByteArray.bytes2File(file: File) = file.writeBytes(this)
+fun ByteArray.bytes2File(file: File) {
+	file.writeBytes(this)
+	FileBytesManager[file.path] = this
+}
 
 fun ByteArray.bytes2File(byteArray: ByteArray, file: String) = bytes2File(File(file))
 
-fun File.readString() = readText()
+fun File.readString() = FileTextManager[path]
 
-fun File.readByteArray() = readBytes()
+fun String.file2String() = FileTextManager[this]
+
+fun File.readByteArray() = FileBytesManager[path]
+
+fun String.file2ByteArray() = FileBytesManager[this]
