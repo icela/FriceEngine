57410bf610406666ed5ed4fd1bb98b11c3b37b63
ice1000 <ice1000kotlin@foxmail.com>
2017-11-05 14:01:54 +0800

    ...

--- src/org/frice/platform/adapter/JfxDrawer.kt
+++ src/org/frice/platform/adapter/JfxDrawer.kt
@@ -1,53 +1,54 @@
 package org.frice.platform.adapter
 
+import javafx.scene.canvas.GraphicsContext
 import org.frice.platform.FriceDrawer
 import org.frice.platform.FriceImage
 import org.frice.resource.graphics.ColorResource
 
-class JfxDrawer(val image: JfxImage) : FriceDrawer {
+class JfxDrawer(val g: GraphicsContext) : FriceDrawer {
 	override val friceImage: FriceImage
-		get() = image
+		get() = TODO()
 	override var color: ColorResource
-		get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+		get() = TODO("not implemented")
 		set(value) = Unit
 
 	override fun init() {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		TODO("not implemented")
 	}
 
 	override fun drawOval(x: Double, y: Double, width: Double, height: Double) {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		TODO("not implemented")
 	}
 
 	override fun drawString(string: String, x: Double, y: Double) {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		TODO("not implemented")
 	}
 
 	override fun drawImage(image: FriceImage, x: Double, y: Double) {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		TODO("not implemented")
 	}
 
 	override fun drawRect(x: Double, y: Double, width: Double, height: Double) {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		TODO("not implemented")
 	}
 
 	override fun drawLine(x: Double, y: Double, width: Double, height: Double) {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		TODO("not implemented")
 	}
 
 	override fun drawRoundRect(x: Double, y: Double, width: Double, height: Double, arcWidth: Double, arcHeight: Double) {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		TODO("not implemented")
 	}
 
 	override fun rotate(theta: Double, x: Double, y: Double) {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		TODO("not implemented")
 	}
 
 	override fun rotate(theta: Double) {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		TODO("not implemented")
 	}
 
 	override fun restore() {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		TODO("not implemented")
 	}
 }
