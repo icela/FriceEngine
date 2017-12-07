33793047f71d50e9d07a32b7b4129bfa7667f4d9
ice1000 <ice1000kotlin@foxmail.com>
2017-12-07 10:47:06 +0800

    [ 1.7.3 ] Update attach object

--- src/org/frice/obj/AttachedObjects.kt
+++ src/org/frice/obj/AttachedObjects.kt
@@ -7,14 +7,15 @@ package org.frice.obj
  * @author ice1000
  * @since v1.7.2
  */
-class AttachedObjects(val objs: MutableList<FObject>) {
+class AttachedObjects<T : FObject>(val objs: MutableList<T>) {
 
 	fun scale(x: Double, y: Double) = objs.forEach { it.scale(x, y) }
 	fun move(x: Double, y: Double) = objs.forEach { it.move(x, y) }
 	fun rotate(angle: Double) = objs.forEach { it.rotate(angle) }
+	operator fun get(x: Int) = objs[x]
 
-	fun addObject(vararg objects: FObject) = objects.forEach { objs.remove(it) }
-	fun removeObject(vararg objects: FObject) = objects.forEach { objs.remove(it) }
+	fun addObject(vararg objects: T) = objects.forEach { objs.remove(it) }
+	fun removeObject(vararg objects: T) = objects.forEach { objs.remove(it) }
 }
 
 /**
@@ -22,9 +23,10 @@ class AttachedObjects(val objs: MutableList<FObject>) {
  * @since v1.7.2
  * @see AttachedObjects
  */
-class AttachedAbstarctObjects(val objs: MutableList<AbstractObject>) {
+class AttachedAbstarctObjects<T : AbstractObject>(val objs: MutableList<T>) {
 	fun move(x: Double, y: Double) = objs.forEach { it.move(x, y) }
+	operator fun get(x: Int) = objs[x]
 
-	fun addObject(vararg objects: FObject) = objects.forEach { objs.remove(it) }
-	fun removeObject(vararg objects: FObject) = objects.forEach { objs.remove(it) }
+	fun addObject(vararg objects: T) = objects.forEach { objs.remove(it) }
+	fun removeObject(vararg objects: T) = objects.forEach { objs.remove(it) }
 }
