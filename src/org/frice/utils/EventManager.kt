bc6af44097b794ddae601734b94ede1c5ab1893e
ice1000 <ice1000kotlin@foxmail.com>
2017-12-07 15:22:19 +0800

    [ #20 ] Finished. Closes #20.

--- src/org/frice/utils/EventManager.kt
+++ src/org/frice/utils/EventManager.kt
@@ -5,6 +5,8 @@ import org.frice.utils.time.FClock
 import java.util.*
 
 /**
+ * Used to manage delayed events.
+ *
  * @author ice1000
  * @since v1.7.4
  */
@@ -13,6 +15,10 @@ class EventManager {
 	fun insert(event: DelayedEvent) = queue.offer(event)
 	fun clear() = queue.clear()
 
+	/**
+	 * Should be invoked in onRefresh.
+	 * It's recommended to invoke this as frequently as possible.
+	 */
 	tailrec fun check() {
 		if (queue.isNotEmpty() && queue.peek().millisFromStart <= FClock.current) {
 			queue.remove().event()
