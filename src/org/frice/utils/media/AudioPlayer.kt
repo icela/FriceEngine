28c7a9aea436672d1b0d87e6b33e489893418f2d
ice1000 <ice1000kotlin@foxmail.com>
2017-12-08 23:54:57 +0800

    [ fix ] Redesign `AudioPlayer`. Java really provides shitty APIs.

--- src/org/frice/utils/media/AudioPlayer.kt
+++ src/org/frice/utils/media/AudioPlayer.kt
@@ -1,7 +1,6 @@
 package org.frice.utils.media
 
 import org.frice.utils.forceRun
-import org.frice.utils.until
 import java.io.File
 import javax.sound.sampled.*
 
@@ -15,58 +14,59 @@ import javax.sound.sampled.*
  */
 class AudioPlayer internal constructor(file: File) : Thread() {
 	internal constructor(path: String) : this(File(path))
+	val runnable = AudioPlayerRunnable(file)
 
-	override fun run() {
-		forceRun(line::open)
-		line.start()
-		var inBytes: Int
-		val audioData = ByteArray(BUFFER_SIZE)
-		until(exited) {
-			inBytes = audioInputStream.read(audioData, 0, BUFFER_SIZE)
-			if (inBytes == -1) return@until
-			line.write(audioData, 0, inBytes)
-		}
-		line.drain()
-		line.close()
-	}
+	override fun run() = runnable.run()
 
 	companion object LineGetter {
 		const val BUFFER_SIZE = 1024
 
 		fun getLine(audioFormat: AudioFormat): SourceDataLine {
-			val info = DataLine.Info(SourceDataLine::class.java, audioFormat)
-			val res = AudioSystem.getLine(info) as SourceDataLine
+			val res = AudioSystem.getLine(DataLine.Info(SourceDataLine::class.java, audioFormat)) as SourceDataLine
 			res.open(audioFormat)
 			return res
 		}
 	}
 
-	private var audioInputStream: AudioInputStream
-	private var format: AudioFormat
-	private var line: SourceDataLine
+	/**
+	 * @author ice1000
+	 * @since v1.7.6
+	 * @see AudioPlayer
+	 */
+	class AudioPlayerRunnable(file: File) : Runnable {
+		private var audioInputStream: AudioInputStream
+		private var format: AudioFormat
+		private var line: SourceDataLine
 
-	init {
-		audioInputStream = AudioSystem.getAudioInputStream(file)
-		format = audioInputStream.format
-		if (format.encoding != AudioFormat.Encoding.PCM_SIGNED) {
-			format = AudioFormat(
-				AudioFormat.Encoding.PCM_SIGNED,
-				format.sampleRate,
-				16,
-				format.channels,
-				format.channels * 2,
-				format.sampleRate,
-				false)
-			audioInputStream = AudioSystem.getAudioInputStream(format, audioInputStream)
+		init {
+			audioInputStream = AudioSystem.getAudioInputStream(file)
+			format = audioInputStream.format
+			if (format.encoding != AudioFormat.Encoding.PCM_SIGNED) {
+				format = AudioFormat(
+					AudioFormat.Encoding.PCM_SIGNED,
+					format.sampleRate,
+					16,
+					format.channels,
+					format.channels shl 1,
+					format.sampleRate,
+					false)
+				audioInputStream = AudioSystem.getAudioInputStream(format, audioInputStream)
+			}
+			line = getLine(format)
 		}
-		line = getLine(format)
 
-	}
-
-	private var exited = false
-
-	fun exit() {
-		exited = true
-		this.join()
+		override fun run() {
+			forceRun(line::open)
+			line.start()
+			var inBytes: Int
+			val audioData = ByteArray(BUFFER_SIZE)
+			while(true) {
+				inBytes = audioInputStream.read(audioData, 0, BUFFER_SIZE)
+				if (inBytes == -1) break
+				line.write(audioData, 0, inBytes)
+			}
+			line.drain()
+			line.close()
+		}
 	}
 }
