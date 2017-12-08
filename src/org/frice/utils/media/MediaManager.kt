ce8c0f766d634fc5f952e4e3bd726359f31aad99
ice1000 <ice1000kotlin@foxmail.com>
2017-12-08 17:05:04 +0800

    [ #29 ] Added @exception KDoc..

--- src/org/frice/utils/media/MediaManager.kt
+++ src/org/frice/utils/media/MediaManager.kt
@@ -7,12 +7,14 @@ import java.nio.file.Paths
 
 
 /**
+ * @exception com.sun.media.jfxmedia.MediaException for mp3/mp4 on Ubuntu 16.04, it doesn't work.
  * @author ice1000
  * @since v1.7.6
  */
 fun playMedia(path: String) = getMediaPlayer(path).play()
 
 /**
+ * @exception com.sun.media.jfxmedia.MediaException for mp3/mp4 on Ubuntu 16.04, it doesn't work.
  * @author ice1000
  * @since v1.7.6
  */
