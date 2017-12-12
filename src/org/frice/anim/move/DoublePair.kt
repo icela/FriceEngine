979d3a23339ff6f18a611d7ebe8ceac8e5235e91
ice1000 <ice1000kotlin@foxmail.com>
2017-12-12 14:00:22 +0800

    [ #34 ] Cleanup `DoublePair`

--- src/org/frice/anim/move/DoublePair.kt
+++ src/org/frice/anim/move/DoublePair.kt
@@ -7,44 +7,4 @@ data class DoublePair(var x: Double, var y: Double) {
 		fun fromM(x: Double, y: Double) = DoublePair(x / 1e6, y / 1e6)
 	}
 
-	operator fun plusAssign(d: DoublePair) {
-		x += d.x
-		y += d.y
-	}
-
-	operator fun plusAssign(d: Double) {
-		x += d
-		y += d
-	}
-
-	operator fun minusAssign(d: DoublePair) {
-		x -= d.x
-		y -= d.y
-	}
-
-	operator fun minusAssign(d: Double) {
-		x -= d
-		y -= d
-	}
-
-	operator fun timesAssign(d: Double) {
-		x *= d
-		y *= d
-	}
-
-	operator fun divAssign(d: Double) {
-		x /= d
-		y /= d
-	}
-
-	operator fun div(d: Double) = DoublePair(x / d, y / d)
-	operator fun times(d: Double) = DoublePair(x * d, y * d)
-	operator fun plus(d: Double) = DoublePair(x + d, y + d)
-	operator fun plus(d: DoublePair) = DoublePair(x + d.x, y + d.y)
-	operator fun minus(d: Double) = DoublePair(x - d, y - d)
-	operator fun minus(d: DoublePair) = DoublePair(x - d.x, y - d.y)
-	operator fun inc() = DoublePair(x++, y++)
-	operator fun dec() = DoublePair(x--, y--)
-	operator fun unaryPlus() = DoublePair(x, y)
-	operator fun unaryMinus() = DoublePair(-x, -y)
 }
--- src/org/frice/anim/move/Move.kt
+++ src/org/frice/anim/move/Move.kt
@@ -7,21 +7,10 @@ import org.frice.obj.AbstractObject
 import org.frice.obj.FObject
 
 /**
- * Created by ice1000 on 2016/8/15.
- * @author ice1000
- * @since v0.2.1
- */
-abstract class MoveAnim : FAnim() {
-	override fun `do`(obj: FObject) = obj.move(delta)
-
-	abstract val delta: DoublePair
-}
-
-/**
  * @author ice1000
  * @since v1.7.2
  */
-abstract class SelfCenteredMoveAnim(val self: AbstractObject) : MoveAnim()
+abstract class SelfCenteredMoveAnim(val self: AbstractObject) : FAnim()
 
 /**
  * Define your own move object as you like.
@@ -30,18 +19,17 @@ abstract class SelfCenteredMoveAnim(val self: AbstractObject) : MoveAnim()
  * @author ice1000
  * @since v0.2.3
  */
-abstract class CustomMove : MoveAnim() {
+abstract class CustomMove : FAnim() {
 	abstract fun getXDelta(timeFromBegin: Double): Double
 	abstract fun getYDelta(timeFromBegin: Double): Double
 
-	override val delta: DoublePair
-		get() {
-			val deltaTime = now - lastRefresh
-			val pair = DoublePair(getXDelta(deltaTime), getYDelta(deltaTime))
-			lastRefresh = now
-			return pair
-		}
+	override fun `do`(obj: FObject) {
+		val deltaTime = now - lastRefresh
+		lastRefresh = now
+		obj.move(getXDelta(deltaTime), getYDelta(deltaTime))
+	}
 }
+
 /**
  * Define your own move object as you like.
  *
@@ -49,13 +37,12 @@ abstract class CustomMove : MoveAnim() {
  * @author ice1000
  * @since v1.7.4
  */
-abstract class CustomMove2 : MoveAnim() {
+abstract class CustomMove2 : FAnim() {
 	abstract fun getDelta(timeFromBegin: Double): DoublePair
 
-	override val delta: DoublePair
-		get() {
-			val pair = getDelta(now - lastRefresh)
-			lastRefresh = now
-			return pair
-		}
+	override fun `do`(obj: FObject) {
+		val pair = getDelta(now - lastRefresh)
+		lastRefresh = now
+		obj.move(pair)
+	}
 }
