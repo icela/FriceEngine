f3ce84c33b9c2e13f87a97bb5698d5225ed8319a
ice1000 <ice1000kotlin@foxmail.com>
2017-12-12 15:49:09 +0800

    [ #32 ] Refactor

--- src/org/frice/utils/media/AudioPlayer.kt
+++ src/org/frice/utils/media/AudioPlayer.kt
@@ -1,5 +1,8 @@
 package org.frice.utils.media
 
+import java.io.File
+import javax.sound.sampled.*
+
 /**
  * From https://github.com/ice1000/Dekoder
  *
@@ -8,13 +11,34 @@ package org.frice.utils.media
  * @since v0.3.1
  * @see org.frice.utils.media.getPlayer
  */
-class AudioPlayer internal constructor(val runnable: AudioPlayerImpl) : Thread(runnable) {
+open class AudioPlayer internal constructor(val file: File) : Thread() {
+	protected var audioInputStream: AudioInputStream
+	private var format: AudioFormat
+	protected var line: SourceDataLine
+	var stopped = false
+
+	init {
+		audioInputStream = AudioSystem.getAudioInputStream(file)
+		format = audioInputStream.format
+		if (format.encoding != AudioFormat.Encoding.PCM_SIGNED) {
+			format = AudioFormat(
+				AudioFormat.Encoding.PCM_SIGNED,
+				format.sampleRate,
+				16,
+				format.channels,
+				format.channels shl 1,
+				format.sampleRate,
+				false)
+			audioInputStream = AudioSystem.getAudioInputStream(format, audioInputStream)
+		}
+		line = `{-# getLine #-}`(format)
+	}
 
 	/**
 	 * It's the safest way to stop playing.
 	 * @since v1.7.7
 	 */
 	fun stopPlaying() {
-		runnable.stopped = true
+		stopped = true
 	}
 }
--- src/org/frice/utils/media/AudioPlayerImpl.kt
+++ src/org/frice/utils/media/AudioPlayerImpl.kt
@@ -2,75 +2,50 @@ package org.frice.utils.media
 
 import org.frice.utils.loop
 import java.io.File
-import javax.sound.sampled.*
 
 /**
  * @author ice1000
  * @since v1.7.6
  * @see org.frice.utils.media.AudioPlayer
  */
-sealed class AudioPlayerImpl(file: File) : Runnable {
-	protected var audioInputStream: AudioInputStream
-	private var format: AudioFormat
-	protected var line: SourceDataLine
-	var stopped = false
-
-	init {
-		audioInputStream = AudioSystem.getAudioInputStream(file)
-		format = audioInputStream.format
-		if (format.encoding != AudioFormat.Encoding.PCM_SIGNED) {
-			format = AudioFormat(
-				AudioFormat.Encoding.PCM_SIGNED,
-				format.sampleRate,
-				16,
-				format.channels,
-				format.channels shl 1,
-				format.sampleRate,
-				false)
-			audioInputStream = AudioSystem.getAudioInputStream(format, audioInputStream)
+class OnceAudioPlayer(file: File) : AudioPlayer(file) {
+	override fun run() {
+		line.open()
+		line.start()
+		var inBytes: Int
+		val audioData = ByteArray(`{-# BUFFER_SIZE #-}`)
+		while (true) {
+			inBytes = audioInputStream.read(audioData, 0, `{-# BUFFER_SIZE #-}`)
+			if (inBytes == -1 || stopped) break
+			line.write(audioData, 0, inBytes)
 		}
-		line = `{-# getLine #-}`(format)
+		audioInputStream.close()
 	}
+}
 
-	// fun close() {
-	// line.drain()
-	// line.close()
-	// }
-
-	class OnceAudioPlayer(file: File) : AudioPlayerImpl(file) {
-		override fun run() {
-			line.open()
-			line.start()
-			var inBytes: Int
-			val audioData = ByteArray(`{-# BUFFER_SIZE #-}`)
-			while (true) {
-				inBytes = audioInputStream.read(audioData, 0, `{-# BUFFER_SIZE #-}`)
-				if (inBytes == -1 || stopped) break
-				line.write(audioData, 0, inBytes)
-			}
-			audioInputStream.close()
+/**
+ * @author ice1000
+ * @since v1.7.6
+ * @see org.frice.utils.media.AudioPlayer
+ */
+class LoopAudioPlayer(file: File) : AudioPlayer(file) {
+	val cache = arrayListOf<ByteArray>()
+	override fun run() {
+		line.open()
+		line.start()
+		var inBytes: Int
+		val audioData = ByteArray(`{-# BUFFER_SIZE #-}`)
+		while (true) {
+			inBytes = audioInputStream.read(audioData, 0, `{-# BUFFER_SIZE #-}`)
+			if (inBytes == -1) break
+			if (stopped) break
+			cache += audioData.clone()
+			line.write(audioData, 0, inBytes)
 		}
-	}
-
-	class LoopAudioPlayer(file: File) : AudioPlayerImpl(file) {
-		val cache = arrayListOf<ByteArray>()
-		override fun run() {
-			line.open()
-			line.start()
-			var inBytes: Int
-			val audioData = ByteArray(`{-# BUFFER_SIZE #-}`)
-			while (true) {
-				inBytes = audioInputStream.read(audioData, 0, `{-# BUFFER_SIZE #-}`)
-				if (inBytes == -1) break
-				if (stopped) break
-				cache += audioData.clone()
-				line.write(audioData, 0, inBytes)
-			}
-			loop block@ {
-				cache.forEachIndexed { index, bytes ->
-					if (stopped) return@block
-					line.write(bytes, 0, if (index == cache.size) inBytes else `{-# BUFFER_SIZE #-}`)
-				}
+		loop block@ {
+			cache.forEachIndexed { index, bytes ->
+				if (stopped) return@block
+				line.write(bytes, 0, if (index == cache.size) inBytes else `{-# BUFFER_SIZE #-}`)
 			}
 		}
 	}
