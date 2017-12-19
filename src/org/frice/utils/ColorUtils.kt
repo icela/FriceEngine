bb7a7a8cd652082606553eb80219e567a495c8e1
ice1000 <ice1000kotlin@foxmail.com>
2017-12-19 12:47:40 +0800

    [ refactor ] Internal refactorings.

--- src/org/frice/utils/ColorUtils.kt
+++ src/org/frice/utils/ColorUtils.kt
@@ -20,7 +20,7 @@ inline fun Color.toJfxColor(): JfxColor = JfxColor.rgb(red, green, blue, alpha /
 inline fun Int.toJfxColor(): JfxColor = JfxColor.rgb(red, green, blue, alpha / 256.0)
 inline fun Int.toAscii() = asciiList[greyify() / (256 / asciiList.size + 1)]
 inline fun Color.average() = (blue + green + red) / 3
-inline fun Int.greyify(): Int = Color(this).average().let { c -> makeColor(c, c, c, alpha) }
+inline fun Int.greyify(): Int = average.let { c -> makeColor(c, c, c, alpha) }
 
 inline fun makeColor(color: Color) = makeColor(color.rgb, color.alpha)
 inline fun makeColor(rgb: Int, alpha: Int) = makeColor(rgb.red, rgb.green, rgb.blue, alpha)
@@ -33,6 +33,6 @@ inline val Int.blue: Int inline get() = this and 255
 inline val Int.green: Int inline get() = this shr 8 and 255
 inline val Int.average inline get() = (blue + green + red) / 3
 
-inline fun Int.greenify(): Int = makeColor(0, Color(this).average(), 0, alpha)
-inline fun Int.redify(): Int = makeColor(Color(this).average(), 0, 0, alpha)
-inline fun Int.bluify(): Int = makeColor(0, 0, Color(this).average(), alpha)
+inline fun Int.greenify(): Int = makeColor(0, average, 0, alpha)
+inline fun Int.redify(): Int = makeColor(average, 0, 0, alpha)
+inline fun Int.bluify(): Int = makeColor(0, 0, average, alpha)
