7f8eced3b9380f316dfa8bbf81f48b7d993325c1
ice1000 <ice1000kotlin@foxmail.com>
2017-12-20 23:41:57 +0800

    [ #56 ] Tested. Closes #56.

---
+++ test/org/frice/obj/VisibilityTest.kt
@@ -0,0 +1,31 @@
+package org.frice.obj
+
+import org.frice.Game
+import org.frice.event.MOUSE_CLICKED
+import org.frice.event.OnMouseEvent
+import org.frice.launch
+import org.frice.obj.sub.ShapeObject
+import org.frice.resource.graphics.ColorResource
+import org.frice.utils.shape.FOval
+
+class VisibilityTest : Game() {
+	val obj = ShapeObject(ColorResource.八云紫, FOval(20.0, 40.0), 0.0, 0.0)
+
+	override fun onLastInit() {
+		setSize(200, 200)
+		addObject(obj)
+	}
+
+	override fun onMouse(e: OnMouseEvent) {
+		if (e.type == MOUSE_CLICKED) {
+			obj.isVisible = !obj.isVisible
+		}
+	}
+
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			launch(VisibilityTest::class.java)
+		}
+	}
+}
