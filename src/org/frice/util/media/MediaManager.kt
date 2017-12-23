f9d319ed1eca00110d1471f67e6d5ab1755e7e3b
ice1000 <ice1000kotlin@foxmail.com>
2017-12-23 22:53:13 +0800

    [ #68 ] Done. Closes #68.

--- src/org/frice/util/media/MediaManager.kt
+++ src/org/frice/util/media/MediaManager.kt
@@ -1,21 +1,30 @@
 @file:JvmName("MediaManager")
+
 package org.frice.util.media
 
-import javafx.scene.media.Media
-import javafx.scene.media.MediaPlayer
+import javafx.scene.media.*
+import org.frice.util.message.FLog
 import java.nio.file.Paths
 
 
 /**
- * @exception com.sun.media.jfxmedia.MediaException for mp3/mp4 on Ubuntu 16.04, it doesn't work.
+ * For mp3/mp4 on Ubuntu 16.04, it doesn't work.
  * @author ice1000
  * @since v1.7.6
  */
-fun playMedia(path: String) = getMediaPlayer(path).play()
+fun playMedia(path: String) {
+	getMediaPlayer(path)?.play()
+}
 
 /**
- * @exception com.sun.media.jfxmedia.MediaException for mp3/mp4 on Ubuntu 16.04, it doesn't work.
+ * For mp3/mp4 on Ubuntu 16.04, it doesn't work.
  * @author ice1000
  * @since v1.7.6
  */
-fun getMediaPlayer(path: String) = MediaPlayer(Media(Paths.get(path).toUri().toString()))
+fun getMediaPlayer(path: String) = try {
+	MediaPlayer(Media(Paths.get(path).toUri().toString()))
+} catch (e: MediaException) {
+	FLog.e("""MediaPlayer doesn't work with mp3/mp4 on Ubuntu 16.04 with OpenJDK 8.
+You may use OpenJDK 9, or switch to other Operating Systems.""")
+	null
+}
